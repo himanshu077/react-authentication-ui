a5a32b2b95a0a5194305e7583e4a982c8744beb1
Himanshu <himanshukumar077@gmail.com>
2024-02-19 19:05:37 +0530

    complete project setup and working on signup page and signin page

---
+++ src/auth/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,17 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const ForgotPassword = () => {
+  return (
+    <Box>
+    <Typography
+      variant="h3"
+      className="!bg-yellow-600 !text-white !font-bold text-2xl p-10"
+    >
+      Forgot password Page
+    </Typography>
+  </Box>
+  )
+}
+
+export default ForgotPassword
